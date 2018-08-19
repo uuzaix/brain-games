1f53418c353043c8ad4d9f1df851f6e60f373024
Larisa Dolgova <larisa.dolgova@futurice.com>
2018-08-19 22:26:06 +0200

    Move methods to the top level. Use imports for libraries

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,38 +1,42 @@
+import scala.util.Random
+import scala.io.StdIn.readLine
+
 object Game {
+  def checkAnswer(answer: String, isEven: Boolean) = answer match {
+    case "stop" => println("The game is stopped")
+    case "no" => {
+      isEven match {
+        case true => println("Wrong!")
+        case false => println("Correct!")
+      }
+      round
+    }
+    case "yes" => {
+      isEven match {
+        case false => println("Wrong!")
+        case true => println("Correct!")
+      }
+      round
+    }
+    case _ => {
+      println("Let's try again")
+      round
+    }
+  }
+
+  def round: Unit = {
+    val r = Random
+    val randomValue = r.nextInt(100)
+    println(s"Question: ${randomValue}")
+    val answer = readLine()
+    val isEven = if (randomValue % 2 == 0) true else false
+    checkAnswer(answer, isEven)
+  }
+
+
   def main(args: Array[String]): Unit = {
     def game = {
-      println("Answer \"yes\" if number even otherwise answer \"no\". Answer \"stop\" to stop the game")
-
-      def round: Unit = {
-        val r = scala.util.Random
-        val randomValue =  r.nextInt(100)
-        println(s"Question: ${randomValue}")
-        val answer = scala.io.StdIn.readLine()
-        val isEven = if (randomValue % 2 == 0) true else false
-        checkAnswer(answer, isEven)
-      }
-
-      def checkAnswer(answer: String, isEven: Boolean) = answer match {
-        case "stop" => println("The game is stopped")
-        case "no" => {
-          isEven match {
-            case true => println("Wrong!")
-            case false => println("Correct!")
-          }
-          round
-        }
-        case "yes" => {
-          isEven match {
-            case false => println("Wrong!")
-            case true => println("Correct!")
-          }
-          round
-        }
-        case _ => {
-          println("Let's try again")
-          round
-        }
-      }
+      println("""Answer "yes" if number even otherwise answer "no". Answer "stop" to stop the game""")
       round
     }
     game
